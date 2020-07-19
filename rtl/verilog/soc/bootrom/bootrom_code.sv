9f66d4e80a983f7b163d57c55fafca7c8c7d7cd5
PacoReinaCampo <frareicam@gmail.com>
2020-07-19 02:48:15 +0200

    new commit

--- rtl/verilog/soc/bootrom/bootrom_code.sv
+++ rtl/verilog/soc/bootrom/bootrom_code.sv
@@ -37,7 +37,7 @@
  *
  * =============================================================================
  * Author(s):
- *   Francisco Javier Reina Campo <frareicam@gmail.com>
+ *   Paco Reina Campo <pacoreinacampo@queenfield.tech>
  */
 
 0 : wb_dat_o =  32'h1820e020;
